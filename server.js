7c1466b36ddc93ad850773529edfc27769596566
Wesley L. Handy <wesleylhandy@gmail.com>
2018-01-06 18:33:52 -0500

    initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const routes = require('./controllers/stock-controllers');
+const routes = require('./controllers/stock-controller');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
