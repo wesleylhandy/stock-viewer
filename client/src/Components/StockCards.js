db87fe972d7af53b57d00b0a70481bc23cd180cf
Wesley L. Handy <wesleylhandy@gmail.com>
2018-01-11 23:20:33 -0500

    initial steps at adding websockets

--- client/src/Components/StockCards.js
+++ client/src/Components/StockCards.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import StockCard from './StockCard';
+import io from 'socket.io-client'; 
 
-export default function StockCards(props) {
-  return <div>{props.stocks.map((stock, i)=><StockCard key={i} symbol={stock.symbol} {...props}/>)}</div>
+// const socket = io('https://ws-api.iextrading.com/1.0/tops'); 
+
+export default class StockCards extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      stocks: props.stocks
+    }
+
+    // socket.on('message', message => console.log(message))
+      
+  }
+  componentDidMount(){
+    // socket.emit('subscribe', this.props.stocks.map(stock=>stock.symbol).join())
+  }
+
+  render(){
+    return <div>{this.state.stocks.map((stock, i)=><StockCard key={i} symbol={stock.symbol} {...this.props}/>)}</div>
+  }
+  
 }
